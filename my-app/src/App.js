9c5a0409d1e222e8503c49b95d088cd0808e5e5b
ledinhthachquang <20521803@gm.uit.edu.vn>
2022-11-28 12:21:57 +0700

    update for overview

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar/Navbar'
+import './App.css';
+import Header from './components/Header/Header';
+import Intro from './components/Intro/Intro';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-    </>
+    <div>
+      <Navbar/>
+      <Header/>
+      <Intro/>
+      <Footer/>
+    </div>
   );
 }
 
---
+++ my-app/src/constants/images.js
@@ -0,0 +1,12 @@
+import bg from '../assets/Bg.svg'
+import hr from '../assets/hero-small.jpg'
+import vl from '../assets/vlgt.png'
+import sb from '../assets/Subtract.png'
+import spoon from '../assets/spoon.svg';
+export default {
+    bg,
+    hr,
+    vl,
+    sb,
+    spoon
+};
