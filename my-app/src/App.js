cbe216abb93a81089e2e6b75dbecc48eeca126fc
ledinhthachquang <20521803@gm.uit.edu.vn>
2022-12-02 23:41:18 +0700

    update intro

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,11 +7,13 @@ import Footer from './components/Footer/Footer';
 import Gallery from './components/Gallery/Gallery';
 import Order from './components/Order/Order';
 import Products from './components/Products/Products';
+import Introduce from './components/introduce/Introduce';
 function App() {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <Introduce/>
       <Products/>
       <Order/>
       <Gallery/>
