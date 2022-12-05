9e3060b70426347733061471aa6aab24364dc1ee
watdadogdoin <90471020+conchodanglamgithe@users.noreply.github.com>
2022-12-05 23:29:23 +0700

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express= require('express')
+const {notFound,errorHandler} = require('./middleware/errorMiddleware')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const productRoutes = require('./routes/productRoutes')
+const app =express()
+var cors = require('cors')
+dotenv.config();
+app.use(cors());
+connectDB();
+
+
+app.get('/',(req,res)=>{
+ res.send('không có cái api nào ở đây hết')
+})
+app.use('/api/products',productRoutes)
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT||5000
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}...`)
+})
