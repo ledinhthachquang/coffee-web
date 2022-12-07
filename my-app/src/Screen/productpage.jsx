6edd86eaaf689c8baeef6dab8983c70fa6df6a82
ledinhthachquang <20521803@gm.uit.edu.vn>
2022-12-07 15:06:05 +0700

    e

--- my-app/src/Screen/productpage.jsx
+++ my-app/src/Screen/productpage.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React,{ useEffect} from 'react'
 import {Row,Col} from 'react-bootstrap'
-import products from '../products'
+import { useDispatch, useSelector} from 'react-redux'
 import Coffee from '../components/Coffee/Coffee'
-const productpage = () => {
+import {listProducts} from '../action/productActions'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+const Productpage = () => {
+  const dispatch = useDispatch()
+  const productList = useSelector(state=>state.productList)
+  const {loading,error,products } =productList
+  useEffect(()=>{
+    dispatch(listProducts())
+  },[dispatch])
+  
   return (
     <>
         <h1>Sản phẩm mới nhất</h1>
-        <Row>
+        {loading ? <Loader/>:error?<Message variant='danger'>{error}</Message>:<Row>
             {products.map(product =>(
                 <Col sm={12} md={6} lg={4} xl={3}>
                     <Coffee product={product}/>
                 </Col>
             ))}
-        </Row>
+        </Row>}
+        
     </>
   )
 }
 
-export default productpage
+export default Productpage
